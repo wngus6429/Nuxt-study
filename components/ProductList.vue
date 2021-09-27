6d18ddfc19380da17cb4e907f71ba7468b8757ec
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2021-09-27 23:29:37 +0900

    update9/27

---
+++ components/ProductList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ products }}</div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  async asyncData() {
+    const response = await axios.get('http://localhost:3000/products')
+    console.log(response)
+    const products = response.data
+    return { products }
+  },
+}
+</script>
+
+<style></style>
--- pages/main.vue
+++ pages/main.vue
@@ -3,14 +3,29 @@
     <!-- <Logo /> -->
     <h1>메인페이지</h1>
     <p>메인페이지 시작합니다.</p>
+    <ProductList />
   </div>
 </template>
 
 <script>
-// import Logo from '../components/NuxtLogo.vue'
+import ProductList from '../components/ProductList.vue'
 export default {
-  // components: {
-  //   Logo,
+  components: { ProductList },
+  // async asyncData() {
+  //   const response = await axios.get('http://localhost:3000/products')
+  //   console.log(response)
+  //   const products = response.data
+  //   return { products }
+  // },
+  // data() {
+  //   return {
+  //     products: [],
+  //   }
+  // },
+  // async created() {
+  //   const response = await axios.get('http://localhost:3000/products')
+  //   console.log(response)
+  //   this.products = response.data
   // },
 }
 </script>
